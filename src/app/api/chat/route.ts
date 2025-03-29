e2c4d89feaf0445ae1c0fffd4f68e3091b4ea5d2
yalla_halim <madihalim@gmail.com>
2025-03-28 17:29:52 -0700

    Update route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: Request) {
     const { messages } = await request.json();
 
     const completion = await openai.chat.completions.create({
-      model: 'ft:gpt-3.5-turbo-0125:personal:the-prophet:BCr6XIKL',
+      model: 'ft:gpt-3.5-turbo-0125:personal:aipoet01:ApKreXiZ',
       messages,
       stream: false,
     });
